10689c5f1416de0275f7ad62c7a820afe0cce75b
James Traver <jtraverjr@gmail.com>
2015-12-05 00:10:14 -0600

    dotenv

---
+++ bot.rb
@@ -0,0 +1,33 @@
+require 'cinch'
+require 'cinch'
+require 'open-uri'
+require 'nokogiri'
+require 'cgi'
+require 'dotenv'
+
+Dotenv.load
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.nick = 'benevolent_bot'
+    c.password =
+    c.server = "irc.freenode.org"
+    c.channels = ["#chicago-oss-devs"]
+  end
+
+  on :message, "hello" do |m|
+    m.reply "<<Hello, #{m.user.nick}>> Welcome to #chicago-oss-devs! I am a bot. Type help."
+  end
+
+  on :message, "help" do |m|
+    m.reply "<<Help>> I have many commands. Type *help* to see this & *newbie* for a new user's guide."
+  end
+
+  on :message, "newbie" do |m|
+    m.reply "<<Newbie>> Visit https://github.com/chicago-oss-devs/chat-channel"
+  end
+
+
+end
+
+bot.start
